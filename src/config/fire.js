a232d6b827cf885d961abf20e74fdf9f34de5ff4
Melvin Towo <melvintowo@gmail.com>
2022-04-28 01:50:33 -0500

    _redirects add for netlify

--- src/config/fire.js
+++ src/config/fire.js
@@ -22,7 +22,7 @@ const app = firebase.initializeApp({
 export const auth = app.auth()
 export default app;
 //Woring one here
-// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -40,3 +40,13 @@ export default app;
   // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   // appId: process.env.REACT_APP_FIREBASE_APP_ID,
   // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+
+
+
+  // apiKey: "AIzaSyDnqJ0DloM1VsBh8Ptw88rfK6Tj4qyPVQU",
+  // authDomain: "toppicks-a81ae.firebaseapp.com",
+  // projectId: "toppicks-a81ae",
+  // storageBucket: "toppicks-a81ae.appspot.com",
+  // messagingSenderId: "42266653025",
+  // appId: "1:42266653025:web:c812671aed905663c21520",
+  // measurementId: "G-55S13WEKR3"
